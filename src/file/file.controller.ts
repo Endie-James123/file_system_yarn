aeceab550d223f5d82a4d293f5a01099688f7f9e
Endie-James123 <ndopnnoabasi@gmail.com>
2024-05-09 13:17:08 +0100

    formatted with prettier

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Post, Req, Res, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { editFileName, imageFileFilter } from './utils';
 import { diskStorage } from 'multer';
 import { FilesInterceptor } from '@nestjs/platform-express';
@@ -6,35 +15,35 @@ import * as fs from 'fs';
 
 @Controller('file')
 export class FileController {
-    @Post('multiple')
-    @UseInterceptors(
-        FilesInterceptor('image',3, {
-            storage: diskStorage({
-                destination:'./src/images',
-                filename: editFileName,
-            }),
-            fileFilter:imageFileFilter,
-        }),
-    )
-    async uploadMultipleFiles(@UploadedFiles()files){
-        const response = [];
-        files.Array.forEach((file) => {
-            const fileResponse = {
-                originalname: file.originalname,
-                filename: file.filename,
-            }
-            response.push(fileResponse)
-        })
-        return response
-    }
+  @Post('multiple')
+  @UseInterceptors(
+    FilesInterceptor('image', 3, {
+      storage: diskStorage({
+        destination: './src/images',
+        filename: editFileName,
+      }),
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.Array.forEach((file) => {
+      const fileResponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileResponse);
+    });
+    return response;
+  }
 
-    @Get(':imgpath')
-    deleteImg(@Param('imgpath')image, @Req()req, @Res() res):Promise<string>{
-        fs.rm('./src/files/' + image, (err) => {
-            if (err){
-                throw err
-            }
-        })
-        return res.end(`${image} successfully deleted`)
-    }
+  @Get(':imgpath')
+  deleteImg(@Param('imgpath') image, @Req() req, @Res() res): Promise<string> {
+    fs.rm('./src/files/' + image, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    return res.end(`${image} successfully deleted`);
+  }
 }
