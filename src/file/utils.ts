d624c74955a02930cfc2ccdbee20c36473f25199
Endie-James123 <ndopnnoabasi@gmail.com>
2024-05-13 20:53:10 +0100

    made more modifications to the imageFileFilter function

--- src/file/utils.ts
+++ src/file/utils.ts
@@ -6,6 +6,9 @@ export const imageFileFilter = (req, file, callback) =>{
     }
     callback(null, true);
 }
+//req: This represents the request object, which contains information about the file upload request.
+//file: This represents the uploaded file itself.
+//callback: This is a function that the imageFileFilter function will call later on to indicate whether the upload is allowed or not.
 
 export const editFileName = (req, file, callback) =>{
     const name = file.originalname.split('.')[0];
@@ -16,3 +19,4 @@ export const editFileName = (req, file, callback) =>{
        .join('');
     callback(null, `${name}-${randomName}.${fileExtName}`);
 }
+
